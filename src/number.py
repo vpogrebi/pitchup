8509aad5f636495277b218bcf3f1e0b903c43844
Valeriy Pogrebitskiy <vpogrebi@iname.com>
2012-05-04 00:06:03 -0400

    Initial check-in - multi-threading implementation
    Change-Id: I7297f68fb307be66c46ed94b06e15d467bc76891

---
+++ src/number.py
@@ -0,0 +1,50 @@
+'''
+Created on May 3, 2012
+
+@author: valeriy
+'''
+
+class Number:
+    def __init__( self, value ):
+        self.value = value
+        self.divisors = self._getDivisors()
+        self.subsets = self._getDivisorSubsets()
+
+    def _getDivisors( self ):
+        "Get a list of all divisors of <self.value> - excluding <self.value> itself"
+        divisors = [1]
+        for num in range( 2, ( self.value / 2 + 1 ) ):
+            if ( self.value % num ) == 0:
+                if num in divisors:
+                    break
+                i = self.value / num
+                if i == num:
+                    divisors.append( num )
+                else:
+                    divisors.extend( [num, self.value / num] )
+
+        return divisors
+
+    def _getDivisorSubsets( self ):
+        "Get a list containing all subsets of <self.value>'s divisors"
+        f = lambda l: reduce( lambda z, x: z + [y + [x] for y in z], l, [[]] )
+        subsets = f( self.divisors )
+        return subsets
+
+    def isOfInterest( self ):
+        """Check if given <self.value> satisfies following requirements:
+            1. sum(self.divisors) > self.value
+            2. No subset from <self.subsets> adds up to exactly <self.value>
+        """
+        isOfInterest = True
+
+        if sum( self.divisors ) > self.value:
+            for subset in self.subsets:
+                if sum( subset ) == self.value:
+                    isOfInterest = False
+                    break
+        else:
+            isOfInterest = False
+
+        return isOfInterest
+
---
+++ src/pitchup.py
@@ -0,0 +1,30 @@
+'''
+Created on May 3, 2012
+
+@author: valeriy
+'''
+
+import time
+import threading
+from number import Number
+
+def checkNum( num ):
+    num = Number( num )
+    if num.isOfInterest():
+        print "Selected pitch: %i (Divisors: %s, Subsets: %s)" % ( num.value, num.divisors, num.subsets )
+
+def pitchup():
+    # Start all the threads - one for each number in range 1 ... 553
+    for num in range( 1, 553 ):
+        thread = threading.Thread( target = checkNum, name = "checkNum_%i" % num, args = ( num, ) )
+        thread.start()
+
+    # Wait for all threads to terminate
+    while threading.activeCount() > 1:
+        time.sleep( 1 )
+
+    return
+
+if __name__ == '__main__':
+    pitchup()
+    print "Done"
