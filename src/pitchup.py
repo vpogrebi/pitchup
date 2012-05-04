365f386f7a9f19542dd8c458b0ae0b2749717e23
Valeriy Pogrebitskiy <vpogrebi@iname.com>
2012-05-04 14:33:14 -0400

    Created powerset() procedure - implementing GENERATOR design pattern. This procedure replaces getDivisorSubsets() method of Number class (number.py), and substitutes generating entire POWERSET of number's divisors list - with the generator that fetches one subset at a time
    Change-Id: Ia2e63a7bf204872a85ffc14782bfb633a6d0deb9

---
+++ src/pitchup.py
@@ -0,0 +1,67 @@
+'''
+Created on May 3, 2012
+
+@author: valeriy
+'''
+
+import time
+import threading
+from number import Number
+
+pith = None
+
+def powerset( seq ):
+    """
+    Returns all the subsets of this set. This is a generator.
+    """
+    if len( seq ) <= 1:
+        yield seq
+        yield []
+    else:
+        for item in powerset( seq[1:] ):
+            yield [seq[0]] + item
+            yield item
+
+def checkNum( num ):
+    """Check if given <num.value> satisfies following requirements:
+        1. sum(num.divisors) > num.value
+        2. No subset from powerset of <num.divisors> adds up to exactly <num.value>
+    """
+    global pitch
+
+    num = Number( num )
+
+    isOfInterest = True
+
+    if sum( num.divisors ) > num.value:
+        subsets = powerset( num.divisors )
+        for subset in subsets:
+            if sum( subset ) == num.value:
+                isOfInterest = False
+                break
+    else:
+        isOfInterest = False
+
+    if isOfInterest:
+        pitch = num.value
+
+def pitchup():
+    "Find all numbers in the range 1 .. 553 that pass requirements"
+
+    # Start all the threads - one for each number in range 1 ... 553
+    for num in range( 1, 553 ):
+        thread = threading.Thread( target = checkNum, name = "checkNum_%i" % num, args = ( num, ) )
+        thread.start()
+
+    # Wait for all threads to terminate
+    while threading.activeCount() > 1:
+        time.sleep( 1 )
+
+    return
+
+if __name__ == '__main__':
+    pitchup()
+    if pitch:
+        print "Selected pitch: %i" % pitch
+    else:
+        print "No pitch selected"
